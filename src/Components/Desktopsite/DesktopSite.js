99cdf8eb9e4b538a2d4cd9a41ea174b5f9fd2a09
SID-DIQ <siddiqdec23@gmail.com>
2023-09-09 21:37:47 +0530

    b

--- src/Components/Desktopsite/DesktopSite.js
+++ src/Components/Desktopsite/DesktopSite.js
@@ -3,11 +3,10 @@ import './DesktopSite.css';
 
 const MobileMessage = () => {
   return (
-    <div className="mobile-message">
-      Please switch to the desktop view to access this content.
+    <div className="mobile-warning">
+      Please switch to the desktop site to access this content.
     </div>
   );
 }
 
 export default MobileMessage;
-
