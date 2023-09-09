3d456ba96e4ba94e6fc06d20ced140d678aacd1c
SID-DIQ <siddiqdec23@gmail.com>
2023-09-09 21:15:07 +0530

    I think final change

---
+++ src/Components/Desktopsite/DesktopSite.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function MobileMessage() {
+  return (
+    <div className="mobile-message">
+      Please switch to the desktop view to access this content.
+    </div>
+  );
+}
+
+export default MobileMessage;
+
