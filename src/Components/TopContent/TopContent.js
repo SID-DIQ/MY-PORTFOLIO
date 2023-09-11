86518d1ef8cdf375852ba439bc51c4e6a6f9d87c
SID-DIQ <siddiqdec23@gmail.com>
2023-09-11 15:30:50 +0530

    s

--- src/Components/TopContent/TopContent.js
+++ src/Components/TopContent/TopContent.js
@@ -6,7 +6,8 @@ const TopContent = () => {
     <div className="topContent">
         <div className="topContent__container">
             <h1>Mr.Mohamed Siddiq </h1>
-            <p>I am a final-year student at Kongunadu College of Engineering and Technology in the department of Electronics and Communication Engineering,
+            <p>I am pursuing my UG in 
+                Kongunadu College of Engineering and Technology in the department of Electronics and Communication Engineering,
                and I am passionate  in information technology.</p>
             <a href="https://drive.google.com/file/d/1AjYJT3OM8zbV-NqiIogvbyLqNWBzU5TS/view?usp=sharing">
             <button className="topContent__downloadButton">Download CV</button>
