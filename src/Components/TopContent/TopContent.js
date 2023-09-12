03762bf3d95816c852ab996c94fb22969497f54e
SID-DIQ <siddiqdec23@gmail.com>
2023-09-12 11:12:34 +0530

    s

--- src/Components/TopContent/TopContent.js
+++ src/Components/TopContent/TopContent.js
@@ -8,8 +8,8 @@ const TopContent = () => {
             <h1>Mr.Mohamed Siddiq </h1>
             <p>
               I am pursuing my UG <br />
-              in Kongunadu College of Engineering and Technology
-              in the department of Electronics and Communication Engineering, and
+              in Kongunadu College of Engineering and Technology <br />
+              in the department of Electronics and Communication Engineering, and <br />
               I am passionate  in information technology.
                 </p>
             <a href="https://drive.google.com/file/d/1AjYJT3OM8zbV-NqiIogvbyLqNWBzU5TS/view?usp=sharing">
