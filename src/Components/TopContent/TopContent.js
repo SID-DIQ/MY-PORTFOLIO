59958b81067b50e1999f302281fc4c73ce38abf8
SID-DIQ <siddiqdec23@gmail.com>
2023-09-13 15:10:17 +0530

    Changes

--- src/Components/TopContent/TopContent.js
+++ src/Components/TopContent/TopContent.js
@@ -8,10 +8,8 @@ const TopContent = () => {
             <h1>Mr.Mohamed Siddiq </h1>
             <p>
             As a dedicated IT professional, I'm driven by my passion for innovation and technology.
-            My journey in IT began with a love for coding and a curiosity for solving complex problems.
-            I thrive on the challenges of this dynamic field and constantly adapt to the latest industry trends.
-            I'm excited about contributing my expertise to forward-thinking companies that value excellence.
-            I seek a workplace that encourages my ideas, fosters continuous learning, and supports personal and professional growth.
+            I'd like to work for a company that would provide me with a platform to expand my abilities
+            and expertise while also assisting me in my company's ongoing growth.
             </p>
             <a href="https://drive.google.com/file/d/1AjYJT3OM8zbV-NqiIogvbyLqNWBzU5TS/view?usp=sharing">
             <button className="topContent__downloadButton">Download CV</button>
