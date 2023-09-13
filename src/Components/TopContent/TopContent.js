8b911fb7e7a7132e0388cd7611ce39766e89457b
SID-DIQ <siddiqdec23@gmail.com>
2023-09-13 09:36:59 +0530

    Top Content Change

--- src/Components/TopContent/TopContent.js
+++ src/Components/TopContent/TopContent.js
@@ -7,11 +7,12 @@ const TopContent = () => {
         <div className="topContent__container">
             <h1>Mr.Mohamed Siddiq </h1>
             <p>
-              I am pursuing my UG <br />
-              in Kongunadu College of Engineering and Technology <br />
-              in the department of Electronics and Communication Engineering, and <br />
-              I am passionate  in information technology.
-              </p>
+            As a dedicated IT professional, I'm driven by my passion for innovation and technology.
+            My journey in IT began with a love for coding and a curiosity for solving complex problems.
+            I thrive on the challenges of this dynamic field and constantly adapt to the latest industry trends.
+            I'm excited about contributing my expertise to forward-thinking companies that value excellence.
+            I seek a workplace that encourages my ideas, fosters continuous learning, and supports personal and professional growth.
+            </p>
             <a href="https://drive.google.com/file/d/1AjYJT3OM8zbV-NqiIogvbyLqNWBzU5TS/view?usp=sharing">
             <button className="topContent__downloadButton">Download CV</button>
         </a>       
